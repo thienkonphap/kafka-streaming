fc477fe3bd2eb450232e5ca4af0dacc1e1049129
thienkonphap <ngocthiennguyen2102@gmail.com>
2025-01-01 07:48:25 +0100

    add docker Airflow

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 import uuid
 from airflow import DAG
@@ -5,7 +6,7 @@ from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'airscholar',
-    'start_date': datetime(2024, 12, 29, 10, 00)
+    'start_date': datetime(2025, 1, 1, 8, 00)
 }
 
 
@@ -18,7 +19,7 @@ def get_data():
 def format_data(res):
     data = {}
     location = res['location']
-    data['id'] = uuid.uuid4()
+    data['id'] = str(uuid.uuid4())
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
@@ -34,13 +35,34 @@ def format_data(res):
 
     return data
 
-# with DAG('user_automation',
-#          default_args=default_args,
-#          schedule='@daily',
-#          catchup=False
-#          ) as dag:
-#     streaming_task = PythonOperator(
-#         task_id='stream_data_from_api',
-#         python_callable=stream_data
-#     )
-print(format_data(get_data()))
+
+def stream_data():
+    import json
+    from kafka import KafkaProducer
+    import time
+    res = get_data()
+    res = format_data(res)
+    print(res)
+
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    current_time = time.time()
+    while True:
+        if time.time() > current_time + 60: # 1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.ERROR(f'An error occured: {e}')
+            continue
+
+with DAG('user_automation',
+         default_args=default_args,
+         schedule='@daily',
+         catchup=False
+         ) as dag:
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
